4b45fe61410b18d65fa03e5f7b519637b852e5f3
Crowdbotics <team@crowdbotics.com>
2024-10-29 03:23:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-ayema-app-demo-49826",
+  url: "https://test-ayema-app-demo-49826.botics.co",
+  api: "https://test-ayema-app-demo-49826.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
